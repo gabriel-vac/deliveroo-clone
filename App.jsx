c420b360ee83b80a78a728a6b5633ddf470c6c75
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2022-07-14 23:07:11 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          {/* {Screens */}
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  );
+}
---
+++ components/Categories.jsx
@@ -0,0 +1,53 @@
+import { View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import CategoryCard from './CategoryCard';
+
+const Categories = () => {
+  return (
+    <ScrollView
+      contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      //   className="bg-red-200"
+    >
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+    </ScrollView>
+  );
+};
+
+export default Categories;
